7a78008fd240abcb57b47602610aa6eb0bff677e
blackbeltbase <54255614+blackbeltbase@users.noreply.github.com>
2020-04-06 11:25:44 -0700

    SlotMachine

--- src/_12_slot_machine/SlotMachine.java
+++ src/_12_slot_machine/SlotMachine.java
@@ -1,38 +1,48 @@
 package _12_slot_machine;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class SlotMachine {
+public class SlotMachine implements ActionListener {
 JFrame throwMoneyAway = new JFrame();
 JPanel panel = new JPanel();
 JButton spin = new JButton();
-	
-	
-	@SuppressWarnings("deprecation")
-	void run() throws MalformedURLException{
+int score = 0;
+JLabel showScore = new JLabel();
+	int reel1Num = 0;
+	int reel2Num = 0;
+	int reel3Num = 0;
+	JLabel reel = null;
+	JLabel reel2 = null;
+	JLabel reel3 = null;
+	void run() throws MalformedURLException {
 		throwMoneyAway.setVisible(true);
+		String toString = Integer.toString(score);
+		showScore.setText("Wins: "+toString);
 		spin.setText("SPIN");
-		
-			JLabel reel = createLabelImage("cherry.image.jpg");
-			reel.resize(200, 200);
+		spin.addActionListener(this);
+			 reel = createReel(1);
 			panel.add(reel); 
-			JLabel reel2 = createLabelImage("lemon.jpg");
-			reel2.resize(200, 200);
+			reel2 = createReel(2);
 			panel.add(reel2); 
-			JLabel reel3 = createLabelImage("orange.jpg");
-			reel3.resize(200, 200);
+			reel3 = createReel(3);
 			panel.add(reel3); 
+			panel.add(showScore);
 		panel.add(spin);
 		throwMoneyAway.add(panel);
 		throwMoneyAway.pack();
+		checkWin();
 	}
 	
 	private JLabel createLabelImage(String fileName) throws MalformedURLException{
@@ -44,9 +54,57 @@ JButton spin = new JButton();
 	Icon icon = new ImageIcon(imageURL);
 
 	JLabel imageLabel = new JLabel(icon);
-	imageLabel.resize(200, 200);
+	imageLabel.setSize(100, 100);
 	return imageLabel;
 }
-//add method to make individual reels
+
+ public JLabel createReel(int whatReel) throws MalformedURLException{
+		JLabel label = null;
+		Random whatPic = new Random();
+		int whatNum = whatPic.nextInt(3);
+		if(whatReel == 1) {
+			this.reel1Num = whatNum;
+		}
+		else if(whatReel == 2) {
+			this.reel2Num = whatNum;
+		}
+		else if(whatReel == 3) {
+			this.reel3Num = whatNum;
+		}
+		
+		
+		if(whatNum == 0) {
+			label = createLabelImage("cherry.image.jpg");
+		}
+		else if(whatNum == 1) {
+			label = createLabelImage("lemon.jpg");
+		}
+		else if(whatNum == 2) {
+			label = createLabelImage("orange.jpg");
+		}
+		return label;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		throwMoneyAway.dispose();
+			try {
+				run();
+			} catch (MalformedURLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+
+	
+	private void checkWin() {
+		if(reel1Num == reel2Num && reel2Num == reel3Num) {
+			JOptionPane.showMessageDialog(null, "You Win");
+			score++;
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "You lose");
+		}
+	}
 	
 }
